97401c81bb0c26a244c8a2496ef9803e97d41ac3
YosefGu <yos770yos@gmail.com>
2025-06-11 17:47:21 +0300

    Adding base structor for menu

---
+++ App/MainApp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Malshinon_.App.Menu;
+
+namespace Malshinon_.App
+{
+    internal class MainApp
+    {
+        public static void StartApp()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                PrintMenu();
+                string select = Console.ReadLine();
+                if (IsValidSelect(select))
+                {
+                    int intSelect = int.Parse(select);
+                    switch (intSelect)
+                    {
+                        case 1:
+                            exit = Report.AddReport();
+                            break;
+                        case 2:
+                            exit = false;
+                            break;
+                        case 3:
+                            exit = false;
+                            break;
+                        case 4:
+                            exit = false;
+                            break;
+                        case 5:
+                            exit = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect choice, please choose a number between 1-5");
+                }
+            }
+            Console.WriteLine("Goodby :)");
+
+        }
+
+        private static void PrintMenu()
+        {
+            Console.WriteLine("1. Enter report");
+            Console.WriteLine("2. Potential candidates for recruitment");
+            Console.WriteLine("3. Potential dangerous targets");
+            Console.WriteLine("4. Notifications");
+            Console.WriteLine("5. Exit");
+        }
+
+        private static bool IsValidSelect(string select)
+        {
+            int intSelect;
+            if (int.TryParse(select, out intSelect))
+            {
+                if (intSelect > 0 && intSelect <= 5)
+                    return true;
+            }
+            return false;
+
+        }
+
+
+
+    }
+}
