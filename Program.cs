84c5ed9a2abd9b23e12177aadac2270957b098f2
YosefGu <yos770yos@gmail.com>
2025-06-12 15:27:23 +0300

    defaolt changes

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Malshinon_.App;
+using Malshinon_.DAL;
+using Malshinon_.utils;
+using MySql.Data.MySqlClient;
 
 namespace Malshinon_
 {
@@ -10,6 +14,18 @@ namespace Malshinon_
     {
         static void Main(string[] args)
         {
+            MainApp.StartApp();
+            //var add = DBUtils.Execute(UsersDAL.CreateUser("David T", GenerateSecretCode()));
+            //var user = DBUtils.Execute(UsersDAL.GetUserBySecretCode(13702));
+            //var data = DBUtils.Execute("SELECT * FROM users");
+
+            //DBUtils.PrintResult(user);
+            //Console.WriteLine(data);
+            //DBUtils.PrintResult(add);
+            //DBUtils.PrintResult(data);
         }
+
+        //move to controllers
+        
     }
 }
