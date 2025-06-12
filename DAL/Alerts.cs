e4eec6bfc75ce32f2ad3dfacc9e26facda351f25
YosefGu <yos770yos@gmail.com>
2025-06-12 15:20:50 +0300

    Adding alert class for sql querys

---
+++ DAL/Alerts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon_.DAL
+{
+    internal class Alerts
+    {
+        public static string CreateAlert(int targetCode, string description)
+        {
+            return $"INSERT INTO alerts (target, message) VALUES ({targetCode}, {description}); ";
+        }
+
+        public static string GetAllAlerts()
+        {
+            return "SELECT * FROM alerts";
+        }
+    }
+}
